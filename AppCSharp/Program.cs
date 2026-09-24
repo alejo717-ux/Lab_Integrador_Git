2265be1e69baad6db3805cb5ee5ee818bc5db538
Alejandro Martínez <alejandrom200120@gmail.com>
2026-09-23 22:58:21 -0500

    Se agrega aplicacion de consola en C#

---
+++ AppCSharp/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AppCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Módulo de C# integrado correctamente.");
+        }
+    }
+}
